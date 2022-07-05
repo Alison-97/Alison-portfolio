1587a7f3534ad0a21bb509477cf4492e4dfba402
AlisonTan <alisontan97@yahoo.com>
2022-07-05 14:15:23 +0800

    mobile responsive

--- mainPage.js
+++ mainPage.js
@@ -1,185 +1,167 @@
-var element = document.getElementById("bus-container");
-var schoolBus = document.getElementById("school-bus");
-var elementHeight = element.clientHeight;
-var windowHeight = window.innerHeight;
-var elementPosition;
+// ANIMATE BUS
 
 document.addEventListener("scroll", animate);
-let scrollY = window.scrollY || window.pageYOffset;
-let scrollPosition = scrollY + windowHeight;
 
-function inView() {
-  elementPosition =
-    element.getBoundingClientRect().top + scrollY + elementHeight / 3;
-  if (scrollPosition > elementPosition) {
-    return true;
-  }
-  return false;
-}
+let element =
+  document.getElementById( "bus-container" ); // so that the road can still be adjust at the end
+let schoolBus = document.getElementById("school-bus");
+let windowHeight = window.innerHeight;
+let scrollY = window.scrollY || window.pageYOffset;
 
 function animate() {
+  // DECLARE VARIABLE
+  let elementHeight = element.clientHeight; // road img is not loaded if elementHeight is declared too early outside this function
+  let journeyLength = elementHeight * 2;
+  let elementPosition = element.getBoundingClientRect().top + window.scrollY;
+  let scrollPosition = window.scrollY + windowHeight;
+  let isInView = scrollPosition > elementPosition;
   let scrollAfterInView = scrollPosition - elementPosition;
-  let firstElementPosition =
-    element.getBoundingClientRect().top +
-    scrollY +
-    elementHeight -
-    elementHeight / 7.5;
-  let firstStop = firstElementPosition < scrollPosition;
-  let scrollAfterFirstStop = scrollPosition - firstElementPosition;
-  let rotate = scrollAfterFirstStop * 1.5;
 
-  let secondStop = null;
-  if (rotate > 45) {
-    secondStop = true;
-  }
+  let firstStop = journeyLength * 0.35 + elementPosition;
+  let secondStop = journeyLength * 0.4 + elementPosition;
+  let thirdStop = journeyLength * 0.45 + elementPosition;
+  let fourthStop = journeyLength * 0.6 + elementPosition;
+  let fifthStop = journeyLength * 0.65 + elementPosition;
+  let sixthStop = journeyLength * 0.7 + elementPosition;
 
-  let thirdStop = null;
-  if (rotate > 90) {
-    thirdStop = true;
-  }
+  let isFirstStop = scrollPosition > firstStop;
+  let isSecondStop = scrollPosition > secondStop;
+  let isThirdStop = scrollPosition > thirdStop;
+  let isFourthStop = scrollPosition > fourthStop;
+  let isFifthStop = scrollPosition > fifthStop;
+  let isSixthStop = scrollPosition > sixthStop;
 
-  let forthElementPosition =
-    element.getBoundingClientRect().top +
-    scrollY +
-    elementHeight +
-    elementHeight / 3.8;
-  let forthStop = forthElementPosition < scrollPosition;
-
-  // move right
-  if (inView()) {
-    let moveRight = scrollAfterInView / 7;
+  // START ANIMATING
+  if (isInView && !isFirstStop) {
+    let moveRight = (scrollAfterInView * 45) / (journeyLength * 0.35);
     schoolBus.style.left = `${moveRight}` + "%";
-    schoolBus.style.transform = "rotate(" + 0 + "deg)";
-    schoolBus.style.top = 0 + "%";
-  }
 
-  //first stop
-  if (firstStop) {
-    schoolBus.style.transform = "rotate(" + rotate + "deg)";
-  }
+    schoolBus.style.top = "0%";
+    schoolBus.style.transform = "rotate(0deg)";
+  } else if (isFirstStop && !isSecondStop) {
+    let scrollAfterFirstStop = scrollPosition - firstStop;
+    let moveRight = (scrollAfterFirstStop * 7) / (0.05 * journeyLength) + 45;
+    schoolBus.style.left = `${moveRight}` + "%";
 
-  if (firstStop && !secondStop) {
-    let static = (13 * scrollAfterInView) / 140;
-
-    var smallMoveRight = scrollAfterInView / 20 + static;
-    schoolBus.style.left = `${smallMoveRight}` + "%";
-
-    let smallSmallMoveDown = scrollAfterFirstStop / 4;
-    schoolBus.style.top = `${smallSmallMoveDown}` + "%";
-  }
-
-  //secondStop
-  if (secondStop) {
-    let static = (13 * scrollAfterInView) / 140;
-    let staticOnFirstStop = scrollAfterInView / 40 + static;
-
-    var smallSmallMoveRight = scrollAfterInView / 40 + staticOnFirstStop;
-    schoolBus.style.left = `${smallSmallMoveRight}` + "%";
-
-    //  console.log("rotate: " + rotate)
-    //  console.log("smallSmallMoveRight: " + smallSmallMoveRight)
-
-    let staticDown = scrollAfterFirstStop * 0.75;
-    let smallMoveDown = scrollAfterFirstStop / 1 - staticDown;
-    schoolBus.style.top = `${smallMoveDown}` + "%";
-  }
-
-  //thirdStop
-  if (thirdStop && !forthStop) {
-    let moveDown;
-    schoolBus.style.transform = "rotate(" + 90 + "deg)";
-    schoolBus.style.left = "57.5%";
-    let anotherStaticDown = scrollAfterFirstStop * 1.75;
-    moveDown = 2 * scrollAfterFirstStop - anotherStaticDown;
+    let moveDown = (scrollAfterFirstStop * 5) / (0.05 * journeyLength);
     schoolBus.style.top = `${moveDown}` + "%";
-  }
 
-  let scrollAfterForthStop = scrollPosition - forthElementPosition;
-  let minusRotate;
-  //forthStop
-  if (forthStop) {
-    minusRotate = 90 - scrollAfterForthStop * 1.5;
-    schoolBus.style.transform = "rotate(" + minusRotate + "deg)";
-    let staticSecond = 58;
-    let smallSmallMoveRightSecond = scrollAfterForthStop / 40 + staticSecond;
-    schoolBus.style.left = `${smallSmallMoveRightSecond}` + "%";
-  }
+    let rotate = (scrollAfterFirstStop * 45) / (0.05 * journeyLength);
+    schoolBus.style.transform = "rotate(" + rotate + "deg)";
+  } else if (isSecondStop && !isThirdStop) {
+    let scrollAfterSecondStop = scrollPosition - secondStop;
+    let moveRight = (scrollAfterSecondStop * 3.5) / (0.05 * journeyLength) + 52;
+    schoolBus.style.left = `${moveRight}` + "%";
 
-  //fifthStop
-  let fifthStop = null;
-  if (minusRotate < 0) {
-    fifthStop = true;
-  }
+    let moveDown = (scrollAfterSecondStop * 15) / (0.05 * journeyLength) + 5;
+    schoolBus.style.top = `${moveDown}` + "%";
 
-  if (fifthStop) {
-    schoolBus.style.transform = "rotate(" + 0 + "deg)";
-    schoolBus.style.top = "79%";
-    let smallMoveRightSecond = scrollAfterForthStop / 10 + 54;
-    schoolBus.style.left = `${smallMoveRightSecond}` + "%";
+    let rotate = (scrollAfterSecondStop * 45) / (0.05 * journeyLength) + 45;
+    schoolBus.style.transform = "rotate(" + rotate + "deg)";
+  } else if (isThirdStop && !isFourthStop) {
+    let scrollAfterThirdStop = scrollPosition - thirdStop;
+    schoolBus.style.left = "55.5%";
+
+    let moveDown = (scrollAfterThirdStop * 40) / (0.15 * journeyLength) + 20;
+    schoolBus.style.top = `${moveDown}` + "%";
+
+    schoolBus.style.transform = "rotate (90deg)";
+  } else if (isFourthStop && !isFifthStop) {
+    let scrollAfterFourthStop = scrollPosition - fourthStop;
+    let moveRight =
+      (scrollAfterFourthStop * 3.5) / (0.05 * journeyLength) + 55.5;
+    schoolBus.style.left = `${moveRight}` + "%";
+
+    let moveDown = (scrollAfterFourthStop * 15) / (0.05 * journeyLength) + 60;
+    schoolBus.style.top = `${moveDown}` + "%";
+
+    let rotate = (scrollAfterFourthStop * -45) / (0.05 * journeyLength) + 90;
+    schoolBus.style.transform = "rotate(" + rotate + "deg)";
+  } else if (isFifthStop && !isSixthStop) {
+    let scrollAfterFifthStop = scrollPosition - fifthStop;
+    let moveRight = (scrollAfterFifthStop * 7) / (0.05 * journeyLength) + 59;
+    schoolBus.style.left = `${moveRight}` + "%";
+
+    let moveDown = (scrollAfterFifthStop * 5) / (0.05 * journeyLength) + 75;
+    schoolBus.style.top = `${moveDown}` + "%";
+
+    let rotate = (scrollAfterFifthStop * -45) / (0.05 * journeyLength) + 45;
+    schoolBus.style.transform = "rotate(" + rotate + "deg)";
+  } else if (isSixthStop) {
+    let scrollAfterSixthStop = scrollPosition - sixthStop;
+    let moveRight = (scrollAfterSixthStop * 34) / (0.3 * journeyLength) + 66;
+    if (moveRight > 100) {
+      moveRight = 100;
+    }
+    schoolBus.style.left = `${moveRight}` + "%";
+    schoolBus.style.top = "80%";
+    schoolBus.style.transform = "rotate(0deg)";
   }
 }
+
+
+// CHANGE DAY / NIGHT MODE
 
 let day = true;
 let body = document.getElementsByTagName("body")[0];
-let link = document.querySelectorAll(".navbar a");
-let about = document.getElementById("about")
-let education = document.getElementById("education")
-let project = document.getElementById("project")
-let projContainer = document.querySelectorAll(".project-container")
-let line4 = document.querySelectorAll(".line4")
+let link = document.querySelectorAll("nav a");
+let about = document.getElementById("about");
+let education = document.getElementById("education");
+let project = document.getElementById("project");
+let projContent = document.querySelectorAll(".project-content");
+let line4 = document.querySelectorAll(".line4");
+let inBar = document.querySelectorAll(".inBar");
 
 function changeMode() {
-  if (day) {
+
+  if (day) {  // MAKE THEM DARK
     body.style.color = "#DAE3F3";
-    link[0].style.color = "#DAE3F3";
-    link[1].style.color = "#DAE3F3";
-    link[2].style.color = "#DAE3F3";
-    link[3].style.color = "#DAE3F3";
+    link.forEach((index) => (index.style.color = "#DAE3F3"));
     about.style.color = "#FFF";
     about.style.textShadow = "0 0 5px black";
     education.style.color = "#445C7F";
     project.style.color = "#FFF";
-    for (let i = 0; i < projContainer.length; i++) {
-      projContainer[i].style.backgroundColor = "#4F4B67";
-      projContainer[i].style.color = "#ECE3E3";
+    for (let i = 0; i < projContent.length; i++) {
+      projContent[i].style.backgroundColor = "#4F4B67";
+      projContent[i].style.color = "#ECE3E3";
     }
     for (let i = 0; i < line4.length; i++) {
       line4[i].style.color = "#FFF";
     }
+
     body.style.backgroundImage = "url(canvas/scenery-night.svg)";
     document.getElementById("mainCanva").data = "canvas/nightMainCanva.html";
     document.getElementById("mode-img").src = "canvas/sun.svg";
     document.getElementById("logo").src = "canvas/logo-bright.svg";
-    
-    
+    console.log(inBar);
+
     day = false;
-  } else if (!day) {
+  } else if (!day) {    // MAKE THEM BRIGHT
     body.style.backgroundImage = "url(canvas/scenery-morning.svg)";
     document.getElementById("mainCanva").data = "canvas/dayMainCanva.html";
     document.getElementById("mode-img").src = "canvas/moon.svg";
     body.style.color = "#445C7F";
-    link[0].style.color = "#445C7F";
-    link[1].style.color = "#445C7F";
-    link[2].style.color = "#445C7F";
-    link[3].style.color = "#445C7F";
-    about.style.color = "#445C7F";
+    link.forEach((index) => (index.style.color = "#445C7F"));
     project.style.color = "#7D759F";
+    about.style.color = "#445c7f";
     about.style.textShadow = "0 0 5px transparent";
-    for (let i = 0; i < projContainer.length; i++) {
-      projContainer[i].style.backgroundColor = "#FFF";
-      projContainer[i].style.color = "#666189";
+    for (let i = 0; i < projContent.length; i++) {
+      projContent[i].style.backgroundColor = "#FFF";
+      projContent[i].style.color = "#666189";
     }
     for (let i = 0; i < line4.length; i++) {
       line4[i].style.color = "#000";
     }
     document.getElementById("logo").src = "canvas/logo.svg";
+
     day = true;
   }
 }
 
 
-function hover(element) {
 
+// CAHNGE CONTACT ICON
+function hover(element) {
   switch (element) {
     case github:
       element.setAttribute("src", "canvas/contact icon/github2.png");
@@ -210,5 +192,32 @@ function unhover(element) {
     case whatsapp:
       element.setAttribute("src", "canvas/contact icon/ws1.png");
       break;
+  }
+}
+
+
+// OPEN MENU
+
+let menu = false;
+
+function openMenu() {
+  if (menu) {
+    // CLOSE IT
+    link.forEach((index) => index.classList.remove("inBar"));
+    document.getElementById("mode-img").style.visibility = "visible";
+    menu = false;
+  } else {
+    // OPEN IT
+    link.forEach((index) => index.classList.add("inBar"));
+    document.getElementById("mode-img").style.visibility = "hidden";
+    menu = true;
+  }
+
+  if (day) {
+    link.forEach((index) => index.classList.add("inBarDay"));
+    link.forEach((index) => index.classList.remove("inBarNight"));
+  } else {
+    link.forEach((index) => index.classList.remove("inBarDay"));
+    link.forEach((index) => index.classList.add("inBarNight"));
   }
 }
